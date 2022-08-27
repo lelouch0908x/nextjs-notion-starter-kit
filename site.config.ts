bf13c75d2ece98ff8c01454889bc0dcf6ae8992b
lelouch0908x <111634145+lelouch0908x@users.noreply.github.com>
2022-08-27 10:32:10 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '31fb0dc2da4f4b67b141d1476a2b33fc',
+  rootNotionPageId: 'fe692efb29aa4ebb97da90b83b437ab0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '小咩理科',
-  domain: 'https://detailed-bakery-29d.notion.site/31fb0dc2da4f4b67b141d1476a2b33fc',
+  domain: 'https://detailed-bakery-29d.notion.site/1-fe692efb29aa4ebb97da90b83b437ab0',
   author: '唐小咩',
 
   // open graph metadata (optional)
